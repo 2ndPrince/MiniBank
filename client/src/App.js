55f6dcf102100e1dffb680bd4e882e9c6d35f951
2ndPrince <astonisher88@gmail.com>
2019-10-12 13:08:49 -0400

    Connected client with server

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Customer from './components/customers/Customer'
+
+import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
@@ -19,74 +21,55 @@ const styles = theme => ({
   }
 });
 
-const customers = [
-  {
-    'customerId': 1,
-    'image': 'https://placeimg.com/64/64/5',
-    'name': 'Youngseok Lee',
-    'ssn': '123456789',
-    'yymmdd': '880405',
-    'address1': '1609 Fairview avenue',
-    'address2': '',
-    'city': 'Royal oak',
-    'state': 'Michigan',
-    'zipCode': '48073',
-    'isActivated': 1
-  },
-  {
-    'customerId': 2,
-    'image': 'https://placeimg.com/64/64/8',
-    'name': 'Jonnie Nason',
-    'ssn': '578204852',
-    'yymmdd': '690530',
-    'address1': '153 Main street',
-    'address2': '',
-    'city': 'Los angeles',
-    'state': 'California',
-    'zipCode': '90073',
-    'isActivated': 1
-  },
-  {
-    'customerId': 3,
-    'image': 'https://placeimg.com/64/64/11',
-    'name': 'Edward General',
-    'ssn': '973659190',
-    'yymmdd': '941102',
-    'address1': '1105 island park blvd',
-    'address2': 'apt 312',
-    'city': 'Shreveport',
-    'state': 'Louisiana',
-    'zipCode': '71105',
-    'isActivated': 1
+class App extends React.Component{
+
+  state ={
+    customers: ""
   }
-]
-function App() {
-  return (
-    <div>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>CUSTOMER ID</TableCell>
-            <TableCell>IMAGE</TableCell>
-            <TableCell>NAME</TableCell>
-            <TableCell>SSN</TableCell>
-            <TableCell>YYMMDD</TableCell>
-            <TableCell>ADDRESS1</TableCell>
-            <TableCell>ADDRESS2</TableCell>
-            <TableCell>CITY</TableCell>
-            <TableCell>STATE</TableCell>
-            <TableCell>ZIPCODE</TableCell>
-            <TableCell>ISACTIVATED</TableCell>
-          </TableRow>
-        </TableHead>
-      <TableBody>
-      {customers.map(c => {
-        return <Customer key={c.customerId} customerId={c.customerId} image={c.image} name={c.name} ssn={c.ssn} yymmdd={c.yymmdd} address1={c.address1} address2={c.address2} city={c.city} state={c.state} zipCode={c.zipCode} isActivated={c.isActivated} />
-      })}
-      </TableBody>
-      </Table>
-    </div>
-  );
+
+  componentDidMount(){
+    this.callApi()
+      .then(res=> this.setState({customers:res}))
+      .catch(err=> console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render(){  
+    const { classes } = this.props;
+    return (
+      <div>
+        <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>CUSTOMER ID</TableCell>
+                <TableCell>IMAGE</TableCell>
+                <TableCell>NAME</TableCell>
+                <TableCell>SSN</TableCell>
+                <TableCell>YYMMDD</TableCell>
+                <TableCell>ADDRESS1</TableCell>
+                <TableCell>ADDRESS2</TableCell>
+                <TableCell>CITY</TableCell>
+                <TableCell>STATE</TableCell>
+                <TableCell>ZIPCODE</TableCell>
+                <TableCell>ISACTIVATED</TableCell>
+              </TableRow>
+            </TableHead>
+          <TableBody>
+          {this.state.customers ? this.state.customers.map(c => {
+            return <Customer key={c.customerId} customerId={c.customerId} image={c.image} name={c.name} ssn={c.ssn} yymmdd={c.yymmdd} address1={c.address1} address2={c.address2} city={c.city} state={c.state} zipCode={c.zipCode} isActivated={c.isActivated} />
+          }) : "" }
+          </TableBody>
+          </Table>
+        </Paper>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,7 +6,55 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/hello', (req, res) => {
-res.send({ message: 'Hello Express!' });
+    res.send({ message: 'Hello Express!' });
 });
+
+
+const customers = [
+    {
+      'customerId': 1,
+      'image': 'https://placeimg.com/64/64/5',
+      'name': 'Youngseok Lee',
+      'ssn': '123456789',
+      'yymmdd': '880405',
+      'address1': '1609 Fairview avenue',
+      'address2': '',
+      'city': 'Royal oak',
+      'state': 'Michigan',
+      'zipCode': '48073',
+      'isActivated': 1
+    },
+    {
+      'customerId': 2,
+      'image': 'https://placeimg.com/64/64/8',
+      'name': 'Jonnie Nason',
+      'ssn': '578204852',
+      'yymmdd': '690530',
+      'address1': '153 Main street',
+      'address2': '',
+      'city': 'Los angeles',
+      'state': 'California',
+      'zipCode': '90073',
+      'isActivated': 1
+    },
+    {
+      'customerId': 3,
+      'image': 'https://placeimg.com/64/64/11',
+      'name': 'Edward General',
+      'ssn': '973659190',
+      'yymmdd': '941102',
+      'address1': '1105 island park blvd',
+      'address2': 'apt 312',
+      'city': 'Shreveport',
+      'state': 'Louisiana',
+      'zipCode': '71105',
+      'isActivated': 1
+    }
+  ]
+
+app.get('/api/customers', (req, res) => {
+    res.send(customers
+    )
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
