262324cefde61bd6aa27b6df6d5dcc7b21448742
2ndPrince <astonisher88@gmail.com>
2019-10-13 04:49:53 -0400

    Implemented finished adding a customer

--- client/src/components/customers/CustomerAdd.js
+++ client/src/components/customers/CustomerAdd.js
@@ -30,6 +30,20 @@ export default class CustomerAdd extends React.Component {
             .then((response) => {
             console.log(response.data); 
         })
+        this.setState({
+            file: null,
+            fileName: "",
+            name: "",
+            ssn: "",
+            yymmdd: "",
+            address1: "",
+            address2: "",
+            city: "",
+            state: "",
+            zipCode: "",
+            isActivated: 0
+        })
+        window.location.reload();
     }
         
     handleFileChange(e) {    
@@ -63,7 +77,7 @@ export default class CustomerAdd extends React.Component {
                 'content-type': 'multipart/form-data'
             }
         }
-        return post(url, formData, config)    
+        return post(url, formData, config);  
     }
         
     render() {
@@ -71,14 +85,14 @@ export default class CustomerAdd extends React.Component {
             <form onSubmit={this.handleFormSubmit}>
                 <h1>Adding a Customer</h1>
                 Profile Image: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange} /><br/>
-                Name: <input type="text" name="userName" value={this.state.name} onChange={this.handleValueChange} /><br/>
-                SSN: <input type="text" name="birthday" value={this.state.ssn} onChange={this.handleValueChange} /><br/>
-                YYMMDD: <input type="text" name="gender" value={this.state.yymmdd} onChange={this.handleValueChange} /><br/>
-                ADDRESS1: <input type="text" name="job" value={this.state.address1} onChange={this.handleValueChange} /><br/>
-                ADDRESS2: <input type="text" name="job" value={this.state.address2} onChange={this.handleValueChange} /><br/>
-                CITY: <input type="text" name="job" value={this.state.city} onChange={this.handleValueChange} /><br/>
-                ZIPCODE: <input type="text" name="job" value={this.state.zipCode} onChange={this.handleValueChange} /><br/>
-                ISACTIVATED: <input type="text" name="job" value={this.state.isActivated} onChange={this.handleValueChange} /><br/>
+                Name: <input type="text" name="name" value={this.state.name} onChange={this.handleValueChange} /><br/>
+                SSN: <input type="text" name="ssn" value={this.state.ssn} onChange={this.handleValueChange} /><br/>
+                YYMMDD: <input type="text" name="yymmdd" value={this.state.yymmdd} onChange={this.handleValueChange} /><br/>
+                ADDRESS1: <input type="text" name="address1" value={this.state.address1} onChange={this.handleValueChange} /><br/>
+                ADDRESS2: <input type="text" name="address2" value={this.state.address2} onChange={this.handleValueChange} /><br/>
+                CITY: <input type="text" name="city" value={this.state.city} onChange={this.handleValueChange} /><br/>
+                ZIPCODE: <input type="text" name="zipCode" value={this.state.zipCode} onChange={this.handleValueChange} /><br/>
+                ISACTIVATED: <input type="text" name="isActivated" value={this.state.isActivated} onChange={this.handleValueChange} /><br/>
                 <button type="submit">Submit</button>
             </form>
 
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
+const multer = require('multer');
+const upload = multer({dest: './upload'})
+
 const connection = mysql.createConnection({
   host: conf.host,
   user: conf.user,
@@ -20,13 +23,32 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-app.get('/api/hello', (req, res) => {
-    res.send({ message: 'Hello Express!' });
-});
-
 app.get('/api/customers', (req, res) => {
   connection.query(
     'SELECT * FROM CUSTOMER',
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  )
+})
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'INSERT INTO CUSTOMER VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  let image = '/image/' + req.file.filename;
+  let name = req.body.name;
+  let ssn = req.body.ssn;
+  let yymmdd = req.body.yymmdd;
+  let address1 = req.body.address1;
+  let address2 = req.body.address2;
+  let city = req.body.city;
+  let zipcode = req.body.zipCode;
+  let state = req.body.state;
+  let isactivated = req.body.isActivated;
+  let params = [image, name, ssn, yymmdd, address1, address2, city, zipcode, state, isactivated];
+
+  connection.query(sql, params,
     (err, rows, fields) => {
       res.send(rows);
     }
